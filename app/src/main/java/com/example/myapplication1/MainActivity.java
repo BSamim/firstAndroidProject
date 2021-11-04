0f0c814f9e5b0e77ab50ef6dea9fb3426db369a7
Samim Zubair <bsamimzubair@gmail.com>
2021-11-04 23:29:51 +0500

    commented extra code

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -7,22 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button button1, share;
-    TextView output;
+public class MainActivity extends AppCompatActivity {
+//    Button button1, share;
+//    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        button1=findViewById(R.id.btn1);
-        share=findViewById(R.id.share);
+//        button1=findViewById(R.id.btn1);
+        //share=findViewById(R.id.share);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void onClick(View view) {
-
-
     }
 
 }
