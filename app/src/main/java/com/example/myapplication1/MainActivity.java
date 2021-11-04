ddc6490c15b0bf5c9b0622554f1e610cd173f196
Samim Zubair <bsamimzubair@gmail.com>
2021-11-05 00:03:57 +0500

    Toast added

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -5,18 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//    Button button1, share;
-//    TextView output;
+    EditText email, pass;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        button1=findViewById(R.id.btn1);
-        //share=findViewById(R.id.share);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=findViewById(R.id.btn);
+        pass=findViewById(R.id.pass);
+        email=findViewById(R.id.email);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),email.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
